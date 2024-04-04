d8cb4c1eb693b19497a17db90f9e089e52227462
amari <amari.lawal@gmail.com>
2024-04-04 21:23:53 +0100

    api key change

--- components/access_token/getaccesstoken.js
+++ components/access_token/getaccesstoken.js
@@ -2,8 +2,8 @@ export const get_access_token = async () =>{
     // https://api.spotify.com/v1/browse/new-releases
     const body = {
         "grant_type":"client_credentials",
-        "client_id": "51c11e01a6e840ab93ae6fa629d7ef69",
-        "client_secret":"13ca561a16c449a495dcaa6367700853"
+        "client_id": "4c278f01163940e18b48f853ccfefadf",
+        "client_secret":"a6227bccdad44817bdef307ececd025f"
     }
     const formBody = Object.keys(body).map(key =>      encodeURIComponent(key) + '=' + encodeURIComponent(body[key])).join('&');
     const response = await fetch('https://accounts.spotify.com/api/token', {
