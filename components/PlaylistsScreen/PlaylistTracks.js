627f3ceb3c6d2f947764f2484d91df03f6a5d3aa
amari <amari.lawal@gmail.com>
2024-06-13 21:35:03 +0100

    playlist edit name works

--- components/PlaylistsScreen/PlaylistTracks.js
+++ components/PlaylistsScreen/PlaylistTracks.js
@@ -15,15 +15,17 @@ import ShowQueue from "../ShowQueue/showqueue";
 
 import { Modal } from "../Tracks/playlistmodal"
 import { Button } from "react-native-elements";
+import Feather from "react-native-vector-icons/Feather"
+import { TextInput } from "react-native-gesture-handler"
 export default function PlaylistTracks({currentTrack,setCurrentTrack,seek, setSeek}){
     const progress = useProgress();
     const location = useLocation();
     const navigate = useNavigate();
     const [trackforplaylist,setTrackForPlaylist] = useState({});
+    const [editingplaylistname,setEditingPlaylistName] = useState(false);
     const { position, duration } = useProgress(200);
     const playerState = usePlaybackState();
     const isPlaying = playerState === State.Playing;
-    console.log(location.state)
     const [playlist_details,setPlaylistDetails] = useState(location.state.playlist_details)
     const [album_tracks,setAlbumTracks] = useState(location.state.playlist_tracks);
     const [loadingaudio,setLoadingAudio] = useState(false)
@@ -35,6 +37,8 @@ export default function PlaylistTracks({currentTrack,setCurrentTrack,seek, setSe
     const [totalpromises,setTotalPromises] = useState(0);
     const [completedpromises,setCompletedPromises] = useState(0);
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isTyping,setIsTyping] = useState(false);
+    const [userinput,setUserInput] = useState("");
 
     const handleModal = () => setIsModalVisible(() => !isModalVisible);
 
@@ -51,6 +55,23 @@ export default function PlaylistTracks({currentTrack,setCurrentTrack,seek, setSe
     const navartistprofile = async () =>{
         //await AsyncStorage.setItem(`artist:${album_tracks[0].artist_name}`,JSON.stringify({"artist_id":album_tracks[0].artist_id}))
         navigate("/artistprofile",{state:album_tracks})
+    }
+    const editplaylistname = async () =>{
+
+
+     
+
+        await AsyncStorage.setItem(`playlist:${userinput}`,JSON.stringify({"playlist_name":userinput,"playlist_thumbnail":playlist_details.playlist_thumbnail,"playlist_size":playlist_details.playlist_size}))
+        let new_playlist_tracks = album_tracks.map((item) =>{return([ `playlist-track:${userinput}-${item.name}`,JSON.stringify(item)])})
+        console.log(new_playlist_tracks)
+        await AsyncStorage.multiSet(new_playlist_tracks)
+        await AsyncStorage.removeItem(`playlist:${playlist_details.playlist_name}`)
+        await AsyncStorage.multiRemove(album_tracks.map((item) =>{return(`playlist-track:${playlist_details.playlist_name}-${item.name}`)}))
+        playlist_details.playlist_name = userinput
+        setPlaylistDetails(playlist_details)
+        setIsTyping(false)
+        setEditingPlaylistName(false);
+
     }
     useEffect(() =>{
         //
@@ -82,8 +103,19 @@ export default function PlaylistTracks({currentTrack,setCurrentTrack,seek, setSe
 
             </TouchableOpacity>
 
-            <View style={{flex:0.1,justifyContent:"center",alignItems:"center"}}>
-                    <Text style={{color:"white",fontSize:20}}>{playlist_details.playlist_name}</Text>
+            <View style={{flex:editingplaylistname === false ? 0.1:isTyping ?0.5: 0.2,justifyContent:"center",alignItems:"center"}}>
+                    <View style={{flexDirection:"row"}}>
+                        {editingplaylistname === false ?
+                        <Text style={{color:"white",fontSize:20}}>{playlist_details.playlist_name}</Text>:
+                        <TextInput onSubmitEditing={() =>{editplaylistname()}} onTouchStart={() =>{setIsTyping(true)}} onEndEditing={() =>{setIsTyping(false)}} style={{width:150}} placeholder="Enter New Playlist" onChangeText={(text) =>{setUserInput(text)}}/>}
+                        {editingplaylistname === false ?
+                                                <TouchableOpacity onPress={() =>{setEditingPlaylistName(true)}}>
+                                                <Feather style={{top:5,left:3}} size={17} name="edit-2"></Feather>
+                                            </TouchableOpacity>:
+                                <TouchableOpacity style={{top:13}} onPress={() =>{setEditingPlaylistName(false);setIsTyping(false)}}>
+                                    <Text>x</Text></TouchableOpacity>}
+
+                    </View>
                     <Text style={{color:"grey",fontSize:15}}>{playlist_details.playlist_size} Tracks</Text>
             </View>
             <FlatList 
