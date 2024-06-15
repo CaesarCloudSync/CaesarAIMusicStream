51280694449c3963edf8c84d9eda6b54d1e52a60
amari <amari.lawal@gmail.com>
2024-06-15 11:54:02 +0100

    change region of the backend to london it worked, made videos work more.

--- components/Tracks/getstreamlinks.js
+++ components/Tracks/getstreamlinks.js
@@ -28,7 +28,7 @@ export const getyoutubelink  = async (album_track,download=false) =>{
 }
 export const getstreaminglink =async (album_track) =>{
     const video_link = await getyoutubelink(album_track,download=false);
-    const response = await axios.get(`https://caesaraimusicstreamyt-qqbn26mgpa-uc.a.run.app/getaudio?url=${video_link}`)
+    const response = await axios.get(`https://caesaraimusicstreamyt-qqbn26mgpa-nw.a.run.app/getaudio?url=${video_link}`)
     let songurl = response.data.streaming_url
     
     return songurl
