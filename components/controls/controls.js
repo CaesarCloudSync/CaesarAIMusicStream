22803f66f8550180a2726b916ae80966900a29d6
amari <amari.lawal@gmail.com>
2024-08-18 16:55:06 +0100

    auto play works

--- components/controls/controls.js
+++ components/controls/controls.js
@@ -4,6 +4,46 @@ import AsyncStorage from "@react-native-async-storage/async-storage"
 import RNFS from "react-native-fs";
 import { get_access_token } from "../access_token/getaccesstoken";
 import { convertToValidFilename } from "../tool/tools";
+import init from 'react_native_mqtt';
+init({
+    size: 10000,
+    storageBackend: AsyncStorage,
+    defaultExpires: 1000 * 3600 * 24,
+    enableCache: true,
+    sync : {}
+  });
+  
+  // 创建客户端实例
+  const options = {
+    host: 'broker.emqx.io',
+    port: 8083,
+    path: '/testTopic',
+    id: 'id_' + parseInt(Math.random()*100000)
+  };
+let client = new Paho.MQTT.Client(options.host, options.port, options.path);
+const sendMessage = async  () =>{
+    const topic = 'caesaraimusicstreamconnect/current-track'
+    let music_connect_next_song = await AsyncStorage.getItem("music_connect_next_track")
+    console.log("sendingmessage",music_connect_next_song)
+    var messagesend = new Paho.MQTT.Message(music_connect_next_song);
+    messagesend.destinationName = topic;
+    client.send(messagesend);
+}
+async function onConnect (){
+
+    console.log('onConnectHello');
+    await sendMessage();
+
+    //sendMessage()
+
+    //this.subscribeTopic()
+  }
+  // 连接失败
+  function onFailure (err) {
+    console.log('Connect failed!');
+    console.log(err);
+    setStatus("failed")
+  }
 const get_thumbnail = async (album_id) =>{
     const access_token = await get_access_token();
     const headers = {Authorization: `Bearer ${access_token}`}
@@ -42,17 +82,42 @@ export const skipToTrack = async (nextsong,player_ind)=>{
         await TrackPlayer.skip(queue.length)
         await TrackPlayer.play()
     }
+    nextsong.url = streaming_link
+    await AsyncStorage.setItem("music_connect_next_track",JSON.stringify(nextsong))
+    await sendmusicconnect()
 
     }
     else{
         var elementPos = queue.findIndex(track => track.id == nextsong.id && track.url !== "dummy")
+        nextsong.url = queue[elementPos].url
         await TrackPlayer.skip(elementPos)
         await TrackPlayer.play()
+        await AsyncStorage.setItem("music_connect_next_track",JSON.stringify(nextsong))
+        await sendmusicconnect()
      
 
     }
 }
+const sendmusicconnect = async () =>{
+  
+
+      if (!client.isConnected()){
+        console.log("music_conncted_connect")
+        client.connect({
+            onSuccess:onConnect,
+            useSSL: false,
+            timeout: 3,
+            onFailure: onFailure
+          });
+      }
+      else{
+        console.log("music_conncted_send")
+        await sendMessage();
+      }
+
+}
 export const autoplaynextsong = async () =>{
+    
 
     const stored_album_tracks = await AsyncStorage.getItem("current-tracks")
     const album_tracks = JSON.parse(stored_album_tracks)
@@ -102,6 +167,7 @@ export const autoplaynextsong = async () =>{
         }
         else{
             await skipToTrack(nextsong,player_ind)
+
             if (track_after_queue){
                 await AsyncStorage.removeItem("track_after_queue")
             }
